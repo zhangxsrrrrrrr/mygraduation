6a2f7cd34f79921f708e5ebe12ff0950ed3f8089
zhangsrrrrrrr <zhangxun04@users.noreply.github.com>
2021-02-09 21:07:17 +0800

    my

---
+++ src/main/java/edu/ahau/graduationproject/dto/AllStudentGradesDTO.java
@@ -0,0 +1,21 @@
+package edu.ahau.graduationproject.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * Created on 2021/2/8.
+ *
+ * @author Xun Zhang
+ *
+ * 查询某一门课所有学生成绩
+ */
+@Data
+public class AllStudentGradesDTO implements Serializable {
+    private static final long serialVersionUID = -7986323315109647110L;
+    private String id;
+    private String username;
+    private String course;
+    private String grade;
+}
